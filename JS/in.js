5567e6798a1bffadf28777c05c21970908e1f5cd
SkorobogatayaKV <55594897+SkorobogatayaKV@users.noreply.github.com>
2020-01-29 16:40:35 +0300

    JS

---
+++ JS/in.js
@@ -0,0 +1,94 @@
+// 1 Номер 
+
+// let x = prompt("Введите номер подъезда");
+
+// if (x >= 1 && x < 21) {
+// 	console.log ("1 подъезд");
+// } else if (x >= 21 && x < 41) {
+// 	console.log("2 подъезд");
+// } else if (x >= 41 && x < 61) {
+// 	console.log("3 подъезд");
+// } else if (x >= 61 && x <81) {
+// 	console.log("4 подъезд");
+// } else if (x >= 81 && x < 101) {
+// 	console.log("5 подъезд");
+// } else if (x < 1 || x > 100) {
+// 	console.log("Данной квартиры нет");
+// }	
+
+// 2 Номер
+
+// let x = prompt("Введите марку автомобиля");
+
+// switch(x) {
+// 	case "BMW":
+// 		console.log ((x) + " – страна происхождения Германия");
+// 		break;
+// 	case "Ford":
+// 		console.log (x + " – страна происхождения США");
+// 		break;
+// 	case "Peugeot":
+// 		console.log ((x) + " – страна происхождения Франция");
+// 		break;
+// 	case "Tesla":
+// 		console.log (x + " – страна происхождения США");
+// 		break;
+// 	case "Toyota":
+// 		console.log (x + " – страна происхождения Япония");
+// 		break;
+// 	case "Mercedes-Benz":
+// 		console.log (x + " – страна происхождения Германия");
+// 		break;
+// 	default:
+// 		console.log(x + "some_input - страна присхождения неизвестна");
+// } 
+
+// 3 Номер
+
+// let x = prompt("Введите год");
+
+// if (x % 4 == 0){
+//    	console.log("високосный");
+// }
+// else{
+// 	console.log("Не високосный")
+// }
+
+//4 Номер
+
+// let x = prompt("Введите число от 1-20");
+// if (x <= 20 && x >= 1){
+// for (let i = 1; i < 11; i++){ 
+// 	console.log(x*i); }
+// }
+// else {
+// 	console.log("Неправильне значение")
+// }
+
+//5 Номер
+
+// let x = 0;
+// for(let i = 1; i < 51; i += 2){
+// 	let x += i;
+// } console.log(x);
+
+// 6 Номер
+
+
+for (let i = 2; i < 18; i++){ 
+	let x= (i-1);
+	let y= (i-2);
+	let m= x + y;
+	console.log(m); }
+
+// else {
+// 	console.log("Неправильне значение")
+// }
+
+
+
+// 7 Номер
+
+
+
+
